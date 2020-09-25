be341ab7f06acffb6da3803728b07613d27f3ba0
ClassUser <ClassUser@spbrtk.local>
2020-09-25 16:28:57 +0300

    init

---
+++ views/site/blog.php
@@ -0,0 +1,4 @@
+<?php foreach ($pp as $post):?>
+<h1><?= $post->name?></h1>
+<h1><?= $post->text?></h1>
+<?php endforeach;?>
---
+++ views/site/create.php
@@ -0,0 +1,23 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Post */
+/* @var $form ActiveForm */
+?>
+
+<div class="create">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'name') ?>
+    <?= $form->field($model, 'text') ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Submit', ['class' => 'btn btn-primary']) ?>
+    </div>
+    <?php ActiveForm::end(); ?>
+
+</div><!-- create -->
